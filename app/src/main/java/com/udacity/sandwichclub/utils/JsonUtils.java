95b4ea4bb13281dc52db4d2f632eab9f0be1cb02
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2018-02-15 20:40:53 +0200

    Fixed activity_details.xml

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,14 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JsonUtils {
 
-    public static Sandwich parseSandwichJson(String json) {
-        return null;
+    public static Sandwich parseSandwichJson(String json) throws JSONException{
+
+        JSONObject sandwichDetails = new JSONObject(json);
+
     }
 }
